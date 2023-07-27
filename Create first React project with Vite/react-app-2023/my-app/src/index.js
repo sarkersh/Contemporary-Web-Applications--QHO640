eec2b071f4211adace849a98e3519051bf379fde
Mohammad Sarker <86519060+sarkersh@users.noreply.github.com>
2023-07-28 00:15:14 +0100

    cc

--- Create first React project with Vite/react-app-2023/my-app/src/index.js
+++ Create first React project with Vite/react-app-2023/my-app/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
+const todoTitle = "Call Family";
+const todoDesc = "dchfchg ghfgh gfhgfg gfhfhgf jhfgg fghgf";
+const date= new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const currentYear= date.getFullYear();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Welcome</h1>
+ReactDOM.render(
+  <div>
+    <h1>Todo App</h1>
+    <h3>{todoTitle}</h3>
+    <p>{todoDesc}</p>
+    <p>{dateName + "/" + monthName + "/" + currentYear}</p>
+  </div>,
+  document.getElementById('root')
+  
 );
